7eb70d770a434561b96a0c9755bd7b2f427ef11f
Harry Tkachenko <tharry@yandex.ru>
2022-12-07 00:00:36 +0300

    +markup

---
+++ script.js
@@ -0,0 +1,175 @@
+// fetch('https://dev.mykgproxy.webprofy.ru/upload/frontend/data.json', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/x-www-form-urlencoded'
+//   },
+//   body: JSON.stringify({
+//     username: 'ivan'
+//   })
+// });
+
+
+// // fetch('https://dev.mykgproxy.webprofy.ru/upload/frontend/data.json')
+// //   .then((res) => {
+// //     console.log(res); // если всё хорошо, получили ответ
+// //   })
+// //   .catch((err) => {
+// //     console.log('Ошибка. Запрос не выполнен');
+// //   });
+
+// fetch('https://dev.mykgproxy.webprofy.ru/upload/frontend/data.json', {
+//   method: 'POST',
+//   body: JSON.stringify({
+//     name: 'Иван',
+//     age: 30
+//   })
+// }); 
+
+const getNews = () => {
+  fetch('https://dev.mykgproxy.webprofy.ru/upload/frontend/data.json', {
+  method: 'POST',
+  body: JSON.stringify({
+    // name: 'Иван',
+    // age: 30
+  })
+}); 
+}
+
+
+
+//   const getNews = () => {
+//   fetch('https://dev.mykgproxy.webprofy.ru/upload/frontend/data.json')
+//     .then((response) => response.json())
+//     .then((res) => {
+//       res.forEach((card) => {
+//         const productCard = new Card(card);
+//         const cardElement = productCard.generateCard();
+//         cardsContainer.prepend(cardElement);
+//         console.log('Выполнено');
+//       });
+//     })
+//     .catch((err) => {
+//       console.log('Ошибка. Запрос не выполнен');
+//     });
+// };
+
+// getFetchProducts();
+// const cardsContainer = document.querySelector('.product__area');
+
+// // Карточка с товаром
+// class Card {
+//   constructor(cardItem) {
+//     this._cardItem = cardItem;
+//     this._cardSelector = '#product-card';
+//     this._productId = cardItem.productId;
+//     this._code = cardItem.code;
+//     this._title = cardItem.title;
+//     this._primaryImageUrl = cardItem.primaryImageUrl;
+//     this._assocProducts = cardItem.assocProducts;
+//     this._priceRetail = cardItem.priceRetail;
+//     this._priceRetailAlt = cardItem.priceRetailAlt;
+//     this._priceGold = cardItem.priceGold;
+//     this._priceGoldAlt = cardItem.priceGoldAlt;
+//     this._bonusAmount = cardItem.bonusAmount;
+//     this.quantity = 0;
+//     this._currentPriceGold = this._priceGold;
+//     this._currentPriceRetail = this._priceRetail;
+//   }
+
+//   _getTemplate = () => {
+//     return document
+//       .querySelector(this._cardSelector)
+//       .content.querySelector('#products_section')
+//       .cloneNode(true);
+//   };
+
+//   // Количество бонусных баллов (~60% полной цены)
+//   _bonusGambler = () => {
+//     return (Math.floor(Math.random() * (this._priceRetail - this._priceGold)) + Math.floor(this._priceGold * .6));
+//   };
+
+//   // Генерация карточки
+//   generateCard = () => {
+//     this._cardItem = this._getTemplate();
+//     this._cardItem.querySelector('.btn_cart').setAttribute('data-product-id', this._productId); // 1.	У кнопки «Купить» должен быть атрибут «data-product-id» с уникальным «id» товара
+//     this._cardItem.querySelector('.product_code').textContent = this._code;
+//     this._cardItem.querySelector('.product_description .product__link').textContent = this._title;
+//     this._cardItem.querySelector('.goldPrice').textContent = `${Math.floor(this._priceGoldAlt)},00`;
+//     this._cardItem.querySelector('.retailPrice').textContent = `${Math.floor(this._priceRetailAlt)},00`;
+//     this._cardItem.querySelector('.product_price_points .ng-binding').textContent = `${'Можно купить за '} ${this._bonusGambler()} балла`;
+
+//     // Добавляет модификатор '_220x220_1' к изображению товара
+//     if (typeof this._primaryImageUrl === 'string') {
+//       const [path, format] = this._primaryImageUrl.split(/\.(?=[^\.]+$)/);
+//       const url = `${path}_220x220_1.${format}`;
+//       this._cardItem.querySelector('.product_photo .product__link .product__link-img').src = url;
+//     }
+
+//     this._addLinks();
+//     this._updateCounter();
+//     this._setEventListeners();
+
+//     return this._cardItem;
+//   };
+
+//   // Добавляет сопутствующие товары
+//   _addLinks() {
+//     const productTags = this._cardItem.querySelector('.product_tags');
+//     const listTextLinks = this._assocProducts.split(';');
+//     listTextLinks.forEach((text) => {
+//       const link = this._createLink(text);
+//       productTags.insertAdjacentElement('beforeend', link);
+//     });
+//   }
+
+//   _createLink(text) {
+//     const link = document.createElement('a');
+//     link.href = '#';
+//     link.textContent = text;
+//     link.classList.add('url--link');
+
+//     return link;
+//   }
+
+//   _setEventListeners() {
+//     this._cardItem.querySelector('.btn_cart').addEventListener('click', () => this._quantityCounterUp());
+//     this._cardItem.querySelector('.up').addEventListener('click', () => this._quantityCounterUp());
+//     this._cardItem.querySelector('.down').addEventListener('click', () => this._quantityCounterDown());
+//     const selectBtns = this._cardItem.querySelectorAll('.unit--select');
+
+//     selectBtns.forEach((btn) => {
+//       btn.addEventListener('click', (evt) => {
+//         const target = evt.currentTarget;
+//         const dataPrice = target.getAttribute('data-price');
+//         utilsRemoveClassForSiblings(target, 'unit--active');
+//         target.classList.add('unit--active');
+//         this._updatePrice(dataPrice);
+//       });
+//     });
+//   }
+
+//   _updatePrice(type) {
+//     this._cardItem.querySelector('.goldPrice').textContent = type === 'alt' ? this._priceGoldAlt : this._priceGold;
+//     this._cardItem.querySelector('.retailPrice').textContent = type === 'alt' ? this._priceRetailAlt : this._priceRetail;
+//   }
+
+//   _quantityCounterUp() {
+//     this.quantity++;
+//     this._updateCounter();
+//   }
+
+//   _quantityCounterDown() {
+//     this.quantity > 0 ? this.quantity-- : 0;
+//     this._updateCounter();
+//   }
+
+//   _updateCounter() {
+//     this._cardItem.querySelector('.stepper-input').value = this.quantity;
+//   }
+// }
+
+// const utilsRemoveClassForSiblings = (target, className) => {
+//   [...target.parentElement.children].forEach((sib) =>
+//     sib.classList.remove(className)
+//   );
+// };
